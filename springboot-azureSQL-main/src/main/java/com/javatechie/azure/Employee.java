f43b15fecf62e03f909f76ac787da62701e20148
Rohit Srivastava <62320661+rcoder23@users.noreply.github.com>
2023-04-01 11:50:07 +0530

    Add files via upload

---
+++ springboot-azureSQL-main/src/main/java/com/javatechie/azure/Employee.java
@@ -0,0 +1,45 @@
+package com.javatechie.azure;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+public class Employee {
+    @Id
+    @GeneratedValue
+    private int id;
+    private String name;
+    private String dept;
+    private long salary;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDept() {
+		return dept;
+	}
+	public void setDept(String dept) {
+		this.dept = dept;
+	}
+	public long getSalary() {
+		return salary;
+	}
+	public void setSalary(long salary) {
+		this.salary = salary;
+	}
+    
+}
